9552b7b66d0e4541827999c2cc29c051caed2a38
ElamTemple <elam9466@gmail.com>
2025-09-20 15:28:57 -0400

    Implemented Champs and Poolsize

--- TftRolldown.py
+++ TftRolldown.py
@@ -1,2 +1,11 @@
-print("Hello world")
-print("Hello")
+import random
+import os
+
+Champs = {
+    1: ["Aatrox", "Ezreal","Garen","Gnar","Kalista","Kayle","Kennen","Lucian","Malphite","Naafiri","Rell","Sivir","Syndra","Zac"],
+    2: ["Dr. Mundo","Gangplank","Janna","Jhin","Kai'sa","Katarina","Kobuko","Lux","Rakan","Shen","Jax","Vi","Xayah","Xin Zhao"],
+    3: ["Ahri","Caitlyn","Darius","Jayce","Lulu","Malzahar","Neeko","Senna","Swain","Udyr","Viego","Yasuo","Ziggs"],
+    4: ["Akali","Ashe","Jarven IV","Jinx","Karma","K'sante","Leona","Poppy","Ryze","Samira","Sett","Volibear","Yuumi"],
+    5: ["Braum","Gwen","Lee Sin","Seraphine","Twisted Fate","Varus","Yone","Zyra"]
+}
+Poolsize = {1: 30*len(Champs[1]), 2: 25*len(Champs[2]), 3:18*len(Champs[3]), 4:10*len(Champs[4]), 5:8*len(Champs[5])}
